c32d3161d5c3fc4c7f4f89e1cdccd5abdefa7b2f
i9code <48942204+i9code@users.noreply.github.com>
2021-01-06 17:34:40 +0800

    YUNKE-2418 【Server】调整yunke-core,yunke文件上传接口，为330提供接口

--- file_api.go
+++ file_api.go
@@ -1,6 +1,9 @@
 package yunke
 
 import (
+	"fmt"
+	"strings"
+
 	class100 "github.com/class100/core"
 	"github.com/class100/yunke-core"
 	`github.com/storezhang/gox`
@@ -8,19 +11,28 @@ import (
 
 type filer interface {
 	// GetUploadFileInfo 获取文件上传信息
-	GetUploadFileInfo(req *core.UploadFileReq) (rsp *core.FileUploadRsp, err error)
+	GetUploadFileInfo(req *core.BaseUploadReq, creatorId int64) (rsp *core.BaseUploadRsp, err error)
 	// GetDownloadFileInfo 获取文件下载信息
-	GetDownloadFileInfo(req *core.GetDownloadReq) (rsp *core.GetDownloadRsp, err error)
+	GetDownloadFileInfo(req *core.BaseDownloadReq) (rsp *core.BaseDownloadRsp, err error)
 	// DeleteFile 删除上传文件
-	DeleteFile(fileId string) (err error)
+	DeleteFile(req *core.BaseFileReq) (err error)
+	// NoticeFileCompletedUpload 通知服务器文件完成上传
+	NoticeFileCompletedUpload(req *core.BaseFileReq) (err error)
 }
 
-func (hsc *httpSignatureClient) GetUploadFileInfo(req *core.UploadFileReq) (rsp *core.FileUploadRsp, err error) {
-	rsp = new(core.FileUploadRsp)
+func (hsc *httpSignatureClient) GetUploadFileInfo(req *core.BaseUploadReq, creatorId int64) (rsp *core.BaseUploadRsp, err error) {
+	rsp = new(core.BaseUploadRsp)
 	params := make(map[string]string)
-	if 0 != len(req.FileId) {
-		params["fileId"] = req.FileId
+	if nil != req.FileId && 0 != len(*req.FileId) {
+		params["fileId"] = *req.FileId
 	}
+	if 0 != len(strings.TrimSpace(req.Filename)) {
+		params["filename"] = req.Filename
+	}
+	params["dataSize"] = fmt.Sprintf("%d", req.DataSize)
+	params["dirType"] = string(req.DirType)
+	params["creatorId"] = fmt.Sprintf("%d", creatorId)
+
 	err = hsc.requestApi(
 		core.FileApiUploadGet,
 		class100.HttpMethodGet,
@@ -32,8 +44,8 @@ func (hsc *httpSignatureClient) GetUploadFileInfo(req *core.UploadFileReq) (rsp 
 	return
 }
 
-func (hsc *httpSignatureClient) GetDownloadFileInfo(req *core.GetDownloadReq) (rsp *core.GetDownloadRsp, err error) {
-	rsp = new(core.GetDownloadRsp)
+func (hsc *httpSignatureClient) GetDownloadFileInfo(req *core.BaseDownloadReq) (rsp *core.BaseDownloadRsp, err error) {
+	rsp = new(core.BaseDownloadRsp)
 	err = hsc.requestApi(
 		core.FileApiDownloadGet,
 		class100.HttpMethodGet,
@@ -46,14 +58,27 @@ func (hsc *httpSignatureClient) GetDownloadFileInfo(req *core.GetDownloadReq) (r
 	return
 }
 
-func (hsc *httpSignatureClient) DeleteFile(fileId string) (err error) {
+func (hsc *httpSignatureClient) DeleteFile(req *core.BaseFileReq) (err error) {
 	err = hsc.requestApi(
 		core.FileApiDelete,
 		class100.HttpMethodDelete,
 		nil,
 		core.ApiVersionDefault,
 		nil,
-		gox.NewHttpPathParameter("fileId", fileId),
+		gox.NewHttpPathParameter("fileId", req.FileId),
+	)
+
+	return
+}
+
+func (hsc *httpSignatureClient) NoticeFileCompletedUpload(req *core.BaseFileReq) (err error) {
+	err = hsc.requestApi(
+		core.FileApiCompletedUploadNotice,
+		class100.HttpMethodPut,
+		nil,
+		core.ApiVersionDefault,
+		nil,
+		gox.NewHttpPathParameter("fileId", req.FileId),
 	)
 
 	return
